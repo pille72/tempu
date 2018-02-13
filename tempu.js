27760db65a661fdd3da7e9416ba02185d7d2d7f1
Philip Majewski <philip.majewski@data-room.de>
2018-02-13 18:41:19 +0100

    [TASK] Restructure code

--- tempu.js
+++ tempu.js
@@ -1,66 +1,7 @@
-const five = require("johnny-five");
-const raspi = require('raspi-io');
-const TempUDisplay = require('./lib/TempuDisplay');
+const TempUController = require('./lib/Controller');
 
-const board = new five.Board({
-    io: new raspi()
+TempUController.init(3000, 0x3c, 4, 11, (data) => {
+    console.log('TempU - Data: ', data);
 });
 
-const display = new TempUDisplay(board, 0x3c, '0', '0');
-
-var rpiDhtSensor = require('rpi-dht-sensor');
-
-var dht = new rpiDhtSensor.DHT11(4);
-
-function read () {
-    var readout = dht.read();
-
-    console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
-        'humidity: ' + readout.humidity.toFixed(2) + '%');
-    setTimeout(read, 5000);
-}
-
-read();
-
-
-board.on('ready', function() {
-    console.log('Connected to Arduino, ready.');
-
-    display.initDisplay();
-    display.update();
-
-    var temp = 30;
-
-    setInterval(() => {
-        display.setTemperature(--temp);
-        display.setHumidity('22.5');
-        display.update();
-    }, 2000);
-
-    var led = new five.Led('P1-11');
-    var led2 = new five.Led('P1-13');
-    var led3 = new five.Led('P1-15');
-    led.on();
-    led2.on();
-    led3.on();
-
-    /*
-    var multi = new five.Multi({
-      io: new raspi(),
-      controller: "DHT22_I2C_NANO_BACKPACK",
-      pin: 4
-    });
-
-    multi.on("change", function() {
-      console.log("Thermometer");
-      console.log("  celsius           : ", this.thermometer.celsius);
-      console.log("  fahrenheit        : ", this.thermometer.fahrenheit);
-      console.log("  kelvin            : ", this.thermometer.kelvin);
-      console.log("--------------------------------------");
-
-      console.log("Hygrometer");
-      console.log("  relative humidity : ", this.hygrometer.relativeHumidity);
-      console.log("--------------------------------------");
-    });
-  */
-});
+TempUController.start();
