e069699143c8c5db16029b8524853b8a98c14f66
Philip Majewski <philip.majewski@data-room.de>
2018-02-24 13:11:34 +0100

    [TASK] Add settings

--- tempu.js
+++ tempu.js
@@ -1,8 +1,7 @@
 const TempUController = require('./lib/Controller');
-const webserver = require('./lib/Webserver')(80);
 const moment = require('moment');
 
-TempUController.init(3000, 0x3c, 4, 22, '28-ef752e126461', 19, 23, 20, 30, 10, webserver, async (data) => {
+TempUController.init(3000, 0x3c, 4, 22, '28-ef752e126461', 19, 23, 20, 30, 10, async (data) => {
     const time = moment().format('YYYY-MM-DD HH:mm:ss');
     console.log(time + ', ' + JSON.stringify(data));
 });
