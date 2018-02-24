90b120d113e706e404daad7cf5199388be972d19
Philip Majewski <philip.majewski@data-room.de>
2018-02-24 11:22:34 +0100

    [TASK] Show time in log

--- tempu.js
+++ tempu.js
@@ -1,8 +1,10 @@
 const TempUController = require('./lib/Controller');
 const webserver = require('./lib/Webserver')(80);
+const moment = require('moment');
 
 TempUController.init(3000, 0x3c, 4, 22, '28-ef752e126461', 19, 23, 20, 30, 10, webserver, async (data) => {
-    console.log('TempU - Data: ', data);
+    const time = moment().format('YYYY-MM-DD HH:mm:ss');
+    console.log(time + ', ' + data);
 });
 
 TempUController.start();
